5ecf4867b6522a3f2c3e6d3a9713880ab228e2e8
Courtney Edmunds <courtney@evometric.com>
2021-12-16 14:14:18 +0000

    favourite fruit addition and add least favourite fruit list

--- fruitlist/fruitlist.py
+++ fruitlist/fruitlist.py
@@ -1,3 +1,5 @@
 # Test for Pushing into Git
 
-favourite_fruit = ["strawberries", "watermelon", "grapes"]
+favourite_fruit = ["strawberries", "watermelon", "grapes", "pomegranate"]
+
+least_favourite_fruit = ["banana", "pear", "oranges"]
