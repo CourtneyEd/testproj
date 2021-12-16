0df50b5596dddcc502073242922483b744507260
Courtney Edmunds <courtney@evometric.com>
2021-12-16 15:43:55 +0000

    Nutrional Feature

---
+++ fruitlist/nutrionallist.py
@@ -0,0 +1,6 @@
+# Random dictionary with fictional values
+# Nutritional Feature for the application
+
+nutrional_dictionary = {"apples":52, "banana":89, "oranges":5}
+
+print(nutrional_dictionary)
--- fruitlist/veglist.py
+++ fruitlist/veglist.py
@@ -1,3 +1,6 @@
+# Additional list of veg
+
+
 favourite_veg = ["carrots", "potato"]
 
 least_favourite_veg = ["swede", "broccoli"]
