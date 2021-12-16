2f386b83ee51c6f26a37cf2fd15c75a4e4193a18
Courtney Edmunds <courtney@evometric.com>
2021-12-16 15:46:39 +0000

    changes to feature and additional dictionary

--- fruitlist/nutrionallist.py
+++ fruitlist/nutrionallist.py
@@ -1,6 +1,10 @@
 # Random dictionary with fictional values
 # Nutritional Feature for the application
 
-nutrional_dictionary = {"apples":52, "banana":89, "oranges":5}
+nutrional_fruit_dictionary = {"apples" : 52, "banana" : 89, "oranges" : 5}
 
-print(nutrional_dictionary)
+nutrional_veg_dictionary = {"broccoli" : -2, "potato" : 105, "carrots" : 274}
+
+print(nutrional_fruit_dictionary)
+
+print(nutrional_veg_dictionary)
